085fa16f0bcfa94ebadac7194c07883092e9d6f3
Tonny H. Soerojo <tonny@soerojo.com>
2017-07-01 06:43:59 +0700

    Modified base_url & dbconnect

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8888/test/';
+$config['base_url'] = 'http://localhost/startup-ci/';
 
 /*
 |--------------------------------------------------------------------------
